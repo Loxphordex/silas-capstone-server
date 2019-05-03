15cfc4cdce80a8b99accd7c57156cb620146994c
Loxphordex <silasishallahan@gmail.com>
2019-05-03 12:15:09 -0700

    working on login

--- src/config.js
+++ src/config.js
@@ -4,5 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
-  QUOTES_API_SECRET: process.env.THEYSAIDSO_API_SECRET
+  QUOTES_API_SECRET: process.env.THEYSAIDSO_API_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
 };
