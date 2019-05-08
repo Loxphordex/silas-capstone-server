df9072d15d83c19b4927151d497f34ea3f36a802
Loxphordex <silasishallahan@gmail.com>
2019-05-08 13:25:33 -0700

    yes cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-//app.use(cors());
+app.use(cors());
 app.use(helmet());
 
 app.use('/api/users', RegisterRouter);
